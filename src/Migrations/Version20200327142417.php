3b6ca100f2cad89428584c0ac702ae837775306d
LAMI <xavier.lami@edu.devinci.fr>
2020-03-27 10:25:06 -0400

    Delete and add migrations

--- src/Migrations/Version20200327142417.php
+++ src/Migrations/Version20200327142417.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200327141453 extends AbstractMigration
+final class Version20200327142417 extends AbstractMigration
 {
     public function getDescription() : string
     {
